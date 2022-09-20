0025979186a6fa78a8e613635db721e4c0dc90d6
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-09-20 20:26:39 +0200

    Add files via upload

---
+++ uloha2.py
@@ -0,0 +1,9 @@
+x=float(input("zadaj mi ciselko:"))
+a=float(input("zaciatok intervalu:"))
+b=float(input("koniec intervalu:"))
+
+if a<x<b:
+    print("ciselko patri do intervalu :)")
+else:
+    print("ciselko nepatri do intervalu :(")
+
