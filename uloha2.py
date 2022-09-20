31150def4ff87ed53ef4811cf9aa243fc2aba481
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-09-20 20:26:22 +0200

    Delete uloha2.py

--- uloha2.py
+++
@@ -1,9 +0,0 @@
-x=float(input("zadaj mi ciselko:"))
-a=float(input("zaciatok intervalu:"))
-b=float(input("koniec intervalu:"))
-
-if a<x<b:
-    print("ciselko patri do intervalu :)")
-else:
-    print("ciselko nepatri do intervalu :(")
-
